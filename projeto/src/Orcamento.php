bb44dc26c4d3351d29164ce6a0cea1d72bda97cf
Marcos Paulo <marcos.poo@gmail.com>
2024-09-24 23:42:28 -0300

    Aula 4.1 - Adicionando estados ao orçamento - correção

--- projeto/src/Orcamento.php
+++ projeto/src/Orcamento.php
@@ -18,5 +18,11 @@ class Orcamento
         if($this->estadoAtual == 'EM_APROVACAO'){
             return $this->valor * 0.05;
         }
+
+        if($this->estadoAtual == 'APROVADO'){
+            return $this->valor * 0.02;
+        }
+
+        throw new \DomainException('Orçamento reprovado e finalizados não podem receber desconto');
     }    
 }
