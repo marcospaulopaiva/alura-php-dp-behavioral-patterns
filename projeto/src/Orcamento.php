6e0b71ff3fdef29522e82fa216382e07ec11115e
Marcos Paulo <marcos.poo@gmail.com>
2024-09-24 23:16:18 -0300

    Aula 4.1 - Adicionando estados ao orçamento

--- projeto/src/Orcamento.php
+++ projeto/src/Orcamento.php
@@ -6,4 +6,17 @@ class Orcamento
 {
     public int $quantidadeItens;
     public float $valor;
+    public string $estadoAtual;
+
+    public function aplicaDescontoExtra()
+    {
+        $this->valor -= $this->calculoDescontoExtra();
+    }
+
+    public function calculoDescontoExtra(): float
+    {
+        if($this->estadoAtual == 'EM_APROVACAO'){
+            return $this->valor * 0.05;
+        }
+    }    
 }
